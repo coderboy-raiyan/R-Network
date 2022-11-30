26c34d0435dc3375a5c66bc1c52975c9aa11eeb9
Raiyan <tajkierhaque@gmail.com>
2022-11-30 12:32:22 +0600

    Some basic changes done

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 require('dotenv').config();
 const colors = require('colors');
 const connectDb = require('./config/db');
-const app = require('./app');
+const app = require('./app/app');
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
